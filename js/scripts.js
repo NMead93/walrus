51765bda6a409b5e3d5fcc2ab4dbad1cb0724f65
nicholas mead and said ahmed <nicholas@>
2017-01-10 09:33:27 -0800

    add jQuery functionality to the rest of the images.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ $(function() {
     $("#show-hide").toggle();
   });
 
+  $(".fade-text").click(function() {
+    $("#fade").fadeToggle();
+  });
 
+  $(".slide-text").click(function() {
+    $("#slide").slideToggle();
+  });
 
 })
